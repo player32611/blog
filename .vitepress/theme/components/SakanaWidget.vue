477c3d29bd0124a183d4e9d68637b82addb7ccc0
player32611 <hcb326630@163.com>
2025-12-04 20:35:09 +0800

    add Koishi1

--- .vitepress/theme/components/SakanaWidget.vue
+++ .vitepress/theme/components/SakanaWidget.vue
@@ -5,10 +5,22 @@
 <script>
     import 'sakana-widget/lib/index.css';
     import SakanaWidget from 'sakana-widget';
+
+    import KoishiImage1 from '/images/Components/Koishi-1.gif';
+    const Koishi1 = SakanaWidget.getCharacter('takina');
+    Koishi1.image = KoishiImage1;
+    Koishi1.initialState = {
+        ...Koishi1.initialState,
+    }
+    SakanaWidget.registerCharacter('Koishi1', Koishi1);
+    
     export default {
         name:"SakanaWidget",
         mounted(){
-            new SakanaWidget().mount('#sakana-widget');
+            new SakanaWidget({ 
+                size: 200,
+                character: 'Koishi1',
+            }).mount('#sakana-widget');
         }
     }
 </script>
