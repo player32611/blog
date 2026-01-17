5c3e4af8a5518f6e1f2e4cb993581a19494aaea2
player32611 <hcb326630@163.com>
2026-01-17 20:07:16 +0800

    误差反向传播法-链式法则和计算图

--- .vitepress/theme/components/SakanaWidget.vue
+++ .vitepress/theme/components/SakanaWidget.vue
@@ -1,30 +1,28 @@
 <template>
-    <div id="sakana-widget" ></div>
+    <div id="sakana-widget"></div>
 </template>
 
 <script>
-    import 'sakana-widget/lib/index.css';
+import 'sakana-widget/lib/index.css';
 
-    export default {
-        name:"SakanaWidget",
-        async mounted(){
-            const [{ default: SakanaWidget }, KoishiImage1] = await Promise.all([
-                import('sakana-widget'),
-                import('/images/Components/Koishi-1.gif?url')  // Vite 会把 ?url 转成字符串
-            ]);
+export default {
+    name: "SakanaWidget",
+    async mounted() {
+        const [{ default: SakanaWidget }, KoishiImage1] = await Promise.all([
+            import('sakana-widget'),
+            import('/images/Components/koishi.gif?url')  // Vite 会把 ?url 转成字符串
+        ]);
 
-            const Koishi1 = SakanaWidget.getCharacter('takina');
-            Koishi1.image = KoishiImage1.default || KoishiImage1; // 兼容两种返回
-            SakanaWidget.registerCharacter('Koishi1', Koishi1);
+        const Koishi1 = SakanaWidget.getCharacter('takina');
+        Koishi1.image = KoishiImage1.default || KoishiImage1; // 兼容两种返回
+        SakanaWidget.registerCharacter('Koishi1', Koishi1);
 
-            new SakanaWidget({ 
-                size: 200,
-                character: 'Koishi1'
-            }).mount('#sakana-widget');
-        }
+        new SakanaWidget({
+            size: 200,
+            character: 'Koishi1'
+        }).mount('#sakana-widget');
     }
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
